d41d8f0b4ab252fcfe4c2b1a8a642d71426644e5
KtosNapewno95 <KtosNapewno95@proton.me>
2026-06-05 09:42:14 +0200

    Flox v0.1.0

--- examples/python/test.py
+++
@@ -1,9 +0,0 @@
-# *tested on python 3.12
-import time
-start_ns = time.perf_counter_ns()
-
-for i in range(10001):
-    print("Siema!")
-
-end_ns = time.perf_counter_ns()
-print(f"\n[Python]: {(end_ns - start_ns) // 1000} mikrosekund")
